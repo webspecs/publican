1aea360f2e5c1d68c8b3e2ab5704f339333043fe
Robin Berjon <robin@berjon.com>
2014-11-12 17:44:38 +0100

    infrastructure for queueing and beginning of integration

--- bin/server.js
+++ bin/server.js
@@ -2,14 +2,12 @@
 var gith = require("gith").create(7002)
 ,   fs = require("fs")
 ,   jn = require("path").join
-,   git = require("../lib/git")
-,   cnf = require("../lib/config")
 ,   man = require("../lib/manager")
-,   rsync = require("../lib/rsync")
 ,   log = require("./log")
+,   queue = require("./queue")
 ,   dataDir = jn(__dirname, "../data")
 ,   wantedFile = jn(dataDir, "wanted.json")
-,   conf = cnf.readConfiguration()
+,   pollInterval = 1000 * 10 // 10 seconds, this is slow, may need to increase later
 ;
 
 // XXX
@@ -23,120 +21,20 @@ gith({})
         var wanted = JSON.parse(fs.readFileSync(wantedFile, "utf8"));
         if (!wanted[repo]) return;
         if (!wanted[repo].branches[branch]) return;
-        function done () {
-            rsync.rsync({
-                    from:   jn(dataDir, "publish/")
-                ,   to:     conf.rsyncRemote + ":" + conf.rsyncPath
-                ,   delete: true
-                }
-            ,   function (err) {
-                    if (err) return log.error(err);
-                    log.info("Hook of "+ repo + "#" + branch + " OK!");
-                }
-            );
-        }
-        var currentCommon
-        ,   regen = wanted[repo].regen
-        ;
-        log.info("Hook regen mode: " + (regen || "default"));
-        if (regen === "extracted") currentCommon = man.commonRepositories();
-        // what was that promises thing people were talking about?
-        git.cloneOrFetch(conf.repoTmpl.replace("{repo}", repo), jn(dataDir, "gits", repo), function (err) {
-            if (err) return log.error(err);
-            git.publish(jn(dataDir, "gits", repo), branch, jn(dataDir, "publish", wanted[repo].branches[branch]), function (err) {
-                if (err) return log.error(err);
-                if (regen === "all") {
-                    var commonRepositories = man.commonRepositories();
-                    man.getRepositories(commonRepositories, conf, function (err) {
-                        if (err) return log.error(err);
-                        man.publishRepositories(commonRepositories, conf, function (err) {
-                            if (err) return log.error(err);
-                            man.bikeshedRepositories(commonRepositories, conf, function (err) {
-                                if (err) return log.error(err);
-                                done();
-                            });
-                        });
-                    });
-                }
-                else if (regen === "extracted") {
-                    // XXX
-                    //  this is not actually what we want
-                    //  we want repo#branch pairs, that's our unit of work
-                    
-                    var newCommon = man.commonRepositories()
-                    ,   oldCache = []
-                    ,   newCache = []
-                    ,   cacheMap = {}
-                    ,   repoCache = function (arr) {
-                            return function (repo) {
-                                for (var k in repo.branches) {
-                                    var str = repo.repository + "#" + k;
-                                    arr.push(str);
-                                    cacheMap[str] = repo.branches[k];
-                                }
-                            };
-                        }
-                    ;
-                    currentCommon.forEach(repoCache(oldCache));
-                    newCommon.forEach(repoCache(newCache));
-                    var deletedRepos = oldCache.filter(function (it) { return newCache.indexOf(it) === -1; })
-                    ,   newRepos = newCache.filter(function (it) { return oldCache.indexOf(it) === -1; })
-                    ,   delFile = jn(dataDir, "deleted.json")
-                    ;
-                    if (deletedRepos.length) {
-                        var delList = [];
-                        if (fs.existsSync(delFile)) delList = JSON.parse(fs.readFileSync(delFile, "utf8"));
-                        fs.writeFileSync(delFile, JSON.stringify(delList.concat(deletedRepos), null, 4), { encoding: "utf8" });
-                        deletedRepos.forEach(function (it) {
-                            var parts = it.split("#", 2)
-                            ,   repo = parts[0]
-                            ,   branch = parts[1]
-                            ;
-                            if (wanted[repo]) {
-                                if (wanted[repo].branches[branch]) {
-                                    delete wanted[repo].branches[branch];
-                                    if (!Object.keys(wanted[repo].branches).length) delete wanted[repo];
-                                }
-                            }
-                        });
-                        fs.writeFileSync(wantedFile, JSON.stringify(wanted, null, 4), { encoding: "utf8" });
-                    }
-                    if (newRepos.length) {
-                        var reposAsWanted = [];
-                        newRepos.forEach(function (it) {
-                            var parts = it.split("#", 2)
-                            ,   repo = parts[0]
-                            ,   branch = parts[1]
-                            ,   obj = {
-                                    repository: repo
-                                ,   branches:   {}
-                                }
-                            ;
-                            obj.branches[branch] = cacheMap[it];
-                            reposAsWanted.push(obj);
-                            if (!wanted[repo]) wanted[repo] = { branches: {} };
-                            wanted[repo].branches[branch] = cacheMap[it];
-                        });
-                        fs.writeFileSync(wantedFile, JSON.stringify(wanted, null, 4), { encoding: "utf8" });
-                        man.getRepositories(reposAsWanted, conf, function (err) {
-                            if (err) return log.error(err);
-                            man.publishRepositories(reposAsWanted, conf, function (err) {
-                                if (err) return log.error(err);
-                                man.bikeshedRepositories(reposAsWanted, conf, function (err) {
-                                    if (err) return log.error(err);
-                                    done();
-                                });
-                            });
-                        });
-                    }
-                }
-                else {
-                    done();
-                }
-            });
-        });
+        var stamp = queue.enqueue(repo, branch);
+        log.info("Queued " + stamp + " for processing.");
     })
 ;
+
+function poll () {
+    var next = queue.next();
+    if (!next) return setInterval(poll, pollInterval);
+    man.processRepository(next, function (err) {
+        if (err) log.error(err);
+        process.nextTick(poll);
+    });
+}
+poll();
 
 // gith.payload({
 //   "ref": "refs/heads/gh-pages",
